35dc1cbc7d235b9e3e7c8bad4022b0f5e0d61a00
Marc <71791731+MKrabs@users.noreply.github.com>
2022-11-04 16:07:16 +0100

    Small redesign

--- blog/admin.py
+++ blog/admin.py
@@ -4,8 +4,9 @@ from django.contrib import admin
 
 from .models import Post, Project, Tag, Comment, Profile
 
-admin.site.register(Post)
+admin.site.register(Profile)
 admin.site.register(Project)
+admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(Comment)
 admin.site.register(Profile)
