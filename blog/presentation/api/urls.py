5ccb847b76bec2a3923a7528933656a71c163b79
magoekce <magoekce@united-internet.de>
2023-05-23 18:33:32 +0200

    feat(API): create blog post per APi

--- blog/presentation/api/urls.py
+++ blog/presentation/api/urls.py
@@ -6,6 +6,7 @@ from blog.presentation.api.profile_api import ProfileAPI
 urlpatterns = [
     path('index/', PostAPI.get_latest_posts),
     path('post/<int:post_id>/', PostAPI.get_post),
+    path('create/', PostAPI.create_post),
 
     path('@<str:user_name>/', ProfileAPI.get_user_profile),
 ]
